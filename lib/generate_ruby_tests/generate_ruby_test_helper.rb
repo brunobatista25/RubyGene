485ee863a52ed5e6a42e4998bb30f2ebe837df92
brunobatista25 <brunobatista101@gmail.com>
2018-09-09 20:56:43 -0300

    edit generate and templates

---
+++ lib/generate_ruby_tests/generate_ruby_test_helper.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+def create_feature_file(name)
+  # opcoes usadas para gerar o arquivo na funcao de modelo
+  opts = { name: camelize(name) }
+  # Thor cria um arquivo com base no modelo templates / feature.tt
+  template('feature', File.join(FileUtils.pwd, 'features', 'specifications',
+                                "#{name.downcase}.feature"), opts)
+end
+
+def create_steps_file(name)
+  # opcoes usadas para gerar o arquivo na funcao de modelo
+  opts = { name: camelize(name) }
+  # Thor cria um arquivo com base no modelo templates / steps.tt
+  template('steps', File.join(FileUtils.pwd, 'features', 'steps_definitions',
+                              "#{name.downcase}_steps.rb"), opts)
+end
+
+def create_page_file(name)
+  # opcoes usadas para gerar o arquivo na funcao de modelo
+  opts = { name: camelize(name) }
+  # Thor cria um arquivo com base no modelo templates / page.tt
+  template('page', File.join(FileUtils.pwd, 'features', 'pages',
+                             "#{name.downcase}_page.rb"), opts)
+end
+
+def create_service_file(name)
+  # opcoes usadas para gerar o arquivo na funcao de modelo
+  opts = { name: camelize(name) }
+  # Thor cria um arquivo com base no modelo templates / services.tt
+  template('services', File.join(FileUtils.pwd, 'features', 'services',
+                                 "#{name.downcase}_services.rb"), opts)
+end
+
+def camelize(string)
+  camelized = ''
+  string.split('_').each do |s|
+    camelized += s.capitalize
+  end
+  camelized
+end
+
+def in_root_project_folder?
+  # Looks if the user is in the root folder of the project
+  unless Dir.exist?(File.join(FileUtils.pwd, 'features', 'specifications'))
+    puts 'Please run this command on the root folder of the project'
+    exit 1
+  end
+  true
+end

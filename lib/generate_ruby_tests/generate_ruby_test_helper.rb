0fff4dd74b69baf00ed7da35df652c9ab573ff2c
brunobatista25 <brunobatista101@gmail.com>
2018-09-09 22:19:04 -0300

    add screens and all generates

--- lib/generate_ruby_tests/generate_ruby_test_helper.rb
+++ lib/generate_ruby_tests/generate_ruby_test_helper.rb
@@ -4,7 +4,7 @@
 def create_feature_file(name)
   # opcoes usadas para gerar o arquivo na funcao de modelo
   opts = { name: camelize(name) }
-  # Thor cria um arquivo com base no modelo templates / feature.tt
+  # Thor cria um arquivo com base no modelo templates/feature.tt
   template('feature', File.join(FileUtils.pwd, 'features', 'specifications',
                                 "#{name.downcase}.feature"), opts)
 end
@@ -12,7 +12,7 @@ end
 def create_steps_file(name)
   # opcoes usadas para gerar o arquivo na funcao de modelo
   opts = { name: camelize(name) }
-  # Thor cria um arquivo com base no modelo templates / steps.tt
+  # Thor cria um arquivo com base no modelo templates/steps.tt
   template('steps', File.join(FileUtils.pwd, 'features', 'steps_definitions',
                               "#{name.downcase}_steps.rb"), opts)
 end
@@ -20,7 +20,7 @@ end
 def create_page_file(name)
   # opcoes usadas para gerar o arquivo na funcao de modelo
   opts = { name: camelize(name) }
-  # Thor cria um arquivo com base no modelo templates / page.tt
+  # Thor cria um arquivo com base no modelo templates/page.tt
   template('page', File.join(FileUtils.pwd, 'features', 'pages',
                              "#{name.downcase}_page.rb"), opts)
 end
@@ -28,9 +28,17 @@ end
 def create_service_file(name)
   # opcoes usadas para gerar o arquivo na funcao de modelo
   opts = { name: camelize(name) }
-  # Thor cria um arquivo com base no modelo templates / services.tt
+  # Thor cria um arquivo com base no modelo templates/services.tt
   template('services', File.join(FileUtils.pwd, 'features', 'services',
                                  "#{name.downcase}_services.rb"), opts)
+end
+
+def create_screens_file(name)
+  # opcoes usadas para gerar o arquivo na funcao de modelo
+  opts = { name: camelize(name) }
+  # Thor cria um arquivo com base no modelo templates/screens.tt
+  template('screens', File.join(FileUtils.pwd, 'features', 'screens',
+                                "#{name.downcase}_screens.rb"), opts)
 end
 
 def camelize(string)
@@ -42,7 +50,7 @@ def camelize(string)
 end
 
 def in_root_project_folder?
-  # Looks if the user is in the root folder of the project
+  # Olha se o usuario esta na pasta raiz do projeto
   unless Dir.exist?(File.join(FileUtils.pwd, 'features', 'specifications'))
     puts 'Please run this command on the root folder of the project'
     exit 1

4b663d246c0e3b83fe9e07d4f0e9e748ef316ffe
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-09-15 09:40:55 -0300

    Update hooks.rb
    corrigindo o name.

--- lib/SkeletonWeb/features/support/hooks.rb
+++ lib/SkeletonWeb/features/support/hooks.rb
@@ -1,7 +1,7 @@
 require_relative 'helper.rb'
 
 After do |scenario|
-  scenario_name = scenario.gsub(/[^A-Za-z ]/, '').gsub(/\s+/, '_')
+  scenario_name = scenario.name.gsub(/[^A-Za-z ]/, '').gsub(/\s+/, '_')
 
   if scenario.failed?
     take_screenshot(scenario_name.downcase!, 'failed')
